c2212e998487b2dfedec40e24ab9cc5182cc1e7f
Bruce Wang <number5@gmail.com>
2018-02-26 11:39:52 +1100

    jackett and transmission

---
+++ transmission/transmission-docker/src/start-transmission.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+SETTINGS=/etc/transmission-daemon/settings.json
+
+if test ! -f ${SETTINGS} ; then
+        cp settings.json /etc/transmission-daemon/
+	# Checks for USERNAME variable
+	if [ -z "$USERNAME" ]; then
+	  echo >&2 'Please set an USERNAME variable (ie.: -e USERNAME=john).'
+	  exit 1
+	fi
+	# Checks for PASSWORD variable
+	if [ -z "$PASSWORD" ]; then
+	  echo >&2 'Please set a PASSWORD variable (ie.: -e PASSWORD=hackme).'
+	  exit 1
+	fi
+	# Modify settings.json
+	sed -i.bak -e "s/#rpc-password#/$PASSWORD/" $SETTINGS
+	sed -i.bak -e "s/#rpc-username#/$USERNAME/" $SETTINGS
+fi
+
+unset PASSWORD USERNAME
+
+exec /usr/bin/transmission-daemon --foreground --config-dir /etc/transmission-daemon
